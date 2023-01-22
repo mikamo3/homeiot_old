12b28811f25433356e9279a5b40e4ee12e24f692
mikamo3 <kamo3proj@gmail.com>
2023-01-23 00:30:12 +0900

    :sparkles: (main) improve log

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from bluepy.btle import Scanner, DefaultDelegate
 import socket
 import json
 import db
+import logging
+from logging import handlers
+import traceback
 
 
 class ScanDelegate(DefaultDelegate):
@@ -10,8 +13,8 @@ class ScanDelegate(DefaultDelegate):
         DefaultDelegate.__init__(self)
 
     def handleDiscovery(self, dev, isNewDev, isNewData):
-        try:
-            for (adtype, desc, value) in dev.getScanData():
+        for (adtype, desc, value) in dev.getScanData():
+            try:
 
                 if (adtype == 22):
                     sensor = device.getDevice(dev.addr, value)
@@ -20,10 +23,18 @@ class ScanDelegate(DefaultDelegate):
                         db.insert(data)
                         s.sendto(json.dumps(data).encode(),
                                  ("127.0.0.1", 1111))
-        except:
-            print("error: {}".format(dev))
+
+            except Exception as e:
+                my_logger.error("{}".format(e))
+                my_logger.error(traceback.format_exc())
 
 
+my_logger = logging.getLogger('MyLogger')
+my_logger.setLevel(logging.DEBUG)
+handler = logging.handlers.SysLogHandler(address='/dev/log')
+my_logger.addHandler(handler)
+
+my_logger.debug("start")
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 db = db.Db()
 scanner = Scanner().withDelegate(ScanDelegate())
